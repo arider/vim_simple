992a35f591a391a3aad4bd1f0437e33570df65fe
arider <andrew.rider@gmail.com>
2017-09-06 16:51:44 -0700

    commenting window swap to test something.

--- setup_vim_simple.sh
+++ setup_vim_simple.sh
@@ -13,4 +13,4 @@ wget https://raw.githubusercontent.com/arider/vim_simple/master/.vimrc -O ~/.vim
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-exec bash
+#exec bash
